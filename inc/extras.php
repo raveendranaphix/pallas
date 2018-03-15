87ac6bc3dabd4230dda6db2bb947d7333a2158df
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-15 10:55:57 +0545

    Added extras.php file

---
+++ inc/extras.php
@@ -0,0 +1,11 @@
+<?php
+
+// Logo URL 
+function pallas_get_logo_url() {
+
+	// Get the image ID
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+
+	$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+	return ( isset( $logo[0] ) ? $logo[0] : '' );
+}
