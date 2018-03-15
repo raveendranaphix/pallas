4cad85aa16f7bd676723991140dd7d1a08a5deac
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-15 15:29:21 +0545

    Modified template-home.php

--- templates/template-home.php
+++ templates/template-home.php
@@ -3,100 +3,13 @@
 	Template Name: Home page layout
 	Template Post Type: page
 */
-get_header(); ?>
+get_header();
 
-	<section class="cd-section">
-		<div class="cd-block">
-			<div class="home-top">
-				<div class="cd-intro">
-					<h1>Great strategy build <br>great <span>brands</span></h1>
-				</div>
-				
-				<div class="small-text">- specialized in brand experience -</div>
-				
-				<div class="social-top">
-					<ul class="list-social">
-						<li class="icon-soc tipped" data-title="twitter"  data-tipper-options='{"direction":"top","follow":"true"}'>
-							<a href="#">&#xf099;</a>
-						</li>
-						<li class="icon-soc tipped" data-title="facebook"  data-tipper-options='{"direction":"top","follow":"true"}'>
-							<a href="#">&#xf09a;</a>
-						</li>
-						<li class="icon-soc tipped" data-title="github"  data-tipper-options='{"direction":"top","follow":"true"}'>
-							<a href="#">&#xf09b;</a>
-						</li>
-						<li class="icon-soc tipped" data-title="google +"  data-tipper-options='{"direction":"top","follow":"true"}'>
-							<a href="#">&#xf0d5;</a>
-						</li>
-					</ul>	
-				</div>		
-				
-				<a href="#scroll-link" class="scroll scroll-down"></a>
-				
-			</div>
-		</div>
-	</section> <!-- .cd-section -->
+	// 
+	get_template_part( 'template-parts/home/content', 'intro' );
+	get_template_part( 'template-parts/home/content', 'section_1' );
+	get_template_part( 'template-parts/home/content', 'section_2' );
+	get_template_part( 'template-parts/home/content', 'section_3' );
+	get_template_part( 'template-parts/home/content', 'section_4' );
 
-	<section class="cd-section" id="scroll-link">
-		<div class="cd-block">
-			<div class="cd-half-block"></div>
-
-			<div class="cd-half-block">
-				<div class="block-text">
-					<h2>About Us<span>.</span></h2>
-					<p>We believe in coming up with original ideas and turning them into digital work that is both innovative and measurable. Taking on thought-provoking projects that challenge us creatively and make us go the extra mile is what we consider a way of life.</p>
-					<a href="about.html" class="btn animsition-link">find out more</a>
-				</div>
-			</div>
-		</div>
-	</section> <!-- .cd-section -->
-
-	<section class="cd-section">
-		<div class="cd-block">
-			<div class="cd-half-block"></div>
-
-			<div class="cd-half-block">
-				<div class="block-text">
-					<h2>Portfolio<span>.</span></h2>
-					<p>We are relentless in moving boundaries and carry out this spirited attitude into digital solutions. Digital solutions that engage, inspire and make you think. Make you curious.</p>
-					<a href="work.html" class="btn animsition-link">discover</a>
-				</div>
-			</div>
-		</div>
-	</section> <!-- .cd-section -->
-
-	<section class="cd-section">
-		<div class="cd-block">
-			<div class="cd-half-block"></div>
-
-			<div class="cd-half-block">
-				<div class="block-text">
-					<h2>Jurnal<span>.</span></h2>
-					<p>Real stories revealed for you, the reader. Cum magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed ligula. Sed volutpat odio non turpis gravida luctus. Praesent elit pede, iaculis facilisis, vehicula mattis, tempus non, arcu.</p>
-					<a href="blog.html" class="btn animsition-link">read articles</a>
-				</div>
-			</div>
-		</div>
-	</section> <!-- .cd-section -->
-
-	<section class="cd-section">
-		<div class="cd-block">
-			<div class="cd-half-block"></div>
-
-			<div class="cd-half-block">
-				<div class="block-text">
-					<h2>Contact<span>.</span></h2>
-					<p>For projects, ideas, collaborations or anything else don’t hesitate to get in touch with us. Or just say hello.</p>
-					<a href="contact.html" class="btn animsition-link">get in touch</a>
-				</div>
-			</div>
-		</div>
-	</section> <!-- .cd-section -->
-	<nav>
-		<ul class="cd-vertical-nav">
-			<li><a href="#0" class="cd-prev inactive"></a></li>
-			<li><a href="#0" class="cd-next inactive1"></a></li>
-		</ul>
-	</nav> <!-- .cd-vertical-nav -->
-
-<?php get_footer();
+get_footer();
