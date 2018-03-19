2381bec9aeb68e536e56a31334848eee3cdf2cb8
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-19 08:54:36 +0545

    Modified homepage section 4

--- template-parts/home/content-section_4.php
+++ template-parts/home/content-section_4.php
@@ -1,17 +1,29 @@
+<?php
+    // Get the page
+    $section_4_page = get_field( 'homepage_section_4', get_the_ID(), true);
+
+    if( !  empty( $section_4_page ) ):
+
+        $image_url = get_the_post_thumbnail_url( $section_4_page->ID, 'full');
+
+?>
+
 <section class="cd-section">
     <div class="cd-block">
-        <div class="cd-half-block"></div>
+        <div class="cd-half-block" style ="background-image: url( <?php echo $image_url; ?> )"></div>
         <div class="cd-half-block">
             <div class="block-text">
-                <h2>Contact<span>.</span></h2>
-                <p>For projects, ideas, collaborations or anything else don’t hesitate to get in touch with us. Or just say hello.</p>
-                <a href="contact.html" class="btn animsition-link">get in touch</a>
+                <h2><?php echo get_the_title( $section_4_page->ID ); ?><span>.</span></h2>
+                <p><?php echo get_the_excerpt( $section_4_page->ID ); ?></p>
+                <a href="<?php echo get_the_permalink( '$section_4_page->ID'); ?>" class="btn animsition-link">
+                    <?php _e( 'get in touch', 'pallas' ); ?>
+                </a>
             </div>
         </div>
     </div>
 </section>
 <!-- .cd-section -->
-
+<?php endif; ?>
 <nav>
     <ul class="cd-vertical-nav">
         <li><a href="#0" class="cd-prev inactive"></a></li>
@@ -19,3 +31,4 @@
     </ul>
 </nav>
 <!-- .cd-vertical-nav -->
+
