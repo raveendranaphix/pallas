e792ca580b4bcc57cb47f62aafb55da6b759e466
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-19 08:16:03 +0545

    Modified  homepage section 3

--- template-parts/home/content-section_3.php
+++ template-parts/home/content-section_3.php
@@ -1,13 +1,25 @@
+<?php
+
+    $section_3_page = get_field( 'homepage_section_3', get_the_ID(), true);
+
+    if( ! empty( $section_3_page ) ) :
+
+        $image_url = get_the_post_thumbnail_url( $section_3_page->ID, 'full');
+
+?>
 <section class="cd-section">
     <div class="cd-block">
-        <div class="cd-half-block"></div>
+        <div class="cd-half-block" style ="background-image: url( <?php echo $image_url; ?> )"></div>
         <div class="cd-half-block">
             <div class="block-text">
-                <h2>Jurnal<span>.</span></h2>
-                <p>Real stories revealed for you, the reader. Cum magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sed ligula. Sed volutpat odio non turpis gravida luctus. Praesent elit pede, iaculis facilisis, vehicula mattis, tempus non, arcu.</p>
-                <a href="blog.html" class="btn animsition-link">read articles</a>
+                <h2><?php echo get_the_title( $section_3_page->ID ); ?><span>.</span></h2>
+                <p><?php echo get_the_excerpt( $section_3_page->ID); ?></p>
+                <a href="<?php echo get_the_permalink( $section_3_page->ID ); ?>" class="btn animsition-link">
+                    <?php _e( 'read articles', 'pallas' ); ?>
+                </a>
             </div>
         </div>
     </div>
 </section>
 <!-- .cd-section -->
+<?php endif;
