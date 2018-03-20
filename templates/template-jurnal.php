d289ec7e95964c74ba311499ead65fac9dd9d7e9
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-20 14:49:35 +0545

    Modified template-jurnal.php

--- templates/template-jurnal.php
+++ templates/template-jurnal.php
@@ -5,6 +5,11 @@
 */
 get_header();
 
-	//
+	// Template parts
+	get_template_part( 'template-parts/jurnal/content', 'intro' );
+
+	foreach ( range( 1, 4 ) as $index ) {
+		get_template_part( 'template-parts/jurnal/content', "section_{$index}" );
+	}
 
 get_footer();
