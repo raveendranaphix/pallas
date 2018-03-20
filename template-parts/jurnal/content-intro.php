f9b3ed2b3cf48702739761c05e209fa185113722
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-20 14:46:58 +0545

    Added new folder jurnal.

---
+++ template-parts/jurnal/content-intro.php
@@ -0,0 +1,25 @@
+<?php
+
+	$image  = get_the_post_thumbnail_url();
+	$text_1 = get_field( 'intro_text_1', get_the_ID(), true );
+	$text_2 = get_field( 'intro_text_2', get_the_ID(), true );
+?>
+<section class="cd-section-blog");>
+	<div class="cd-block-blog">
+		<div class="blog-top" style ="background-image: url( <?php echo $image; ?>">
+
+			<?php if( ! empty( $text_1 ) ): ?>
+				<div class="big-text-pages-top">
+					<?php echo sprintf( '<h1>%s<span>.</span></h1>', $text_1 ); ?>
+				</div>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $text_2 ) ): ?>
+				<div class="small-text"><?php echo $text_2; ?></div>
+			<?php endif; ?>
+
+			<a href="#scroll-link" class="scroll scroll-down"></a>
+
+		</div>
+	</div>
+</section> <!-- .cd-section -->
---
+++ template-parts/jurnal/content-section_1.php
@@ -0,0 +1,32 @@
+<?php
+
+	//
+	$section_1_post = get_field( 'jurnal_section_1', get_the_ID(), true );
+
+    if ( ! empty( $section_1_post ) ) :
+
+		setup_postdata( $GLOBALS['post'] =& $section_1_post );
+		global $post;
+
+    	// Image
+        $image_url = get_the_post_thumbnail_url( $post->ID, 'full'  );
+?>
+<section class="cd-section-blog" id="scroll-link">
+	<div class="cd-block-blog">
+		<div class="cd-half-block-blog" style="background-image: url(<?php echo $image_url; ?>);"></div>
+		<div class="cd-half-block-blog">
+			<div class="block-text-blog">
+				<div class="blog-date">
+					<?php echo get_the_date( 'F j, Y', $post->ID ); ?>
+					<span>Author: <?php echo get_the_author(); ?></span>
+				</div>
+				<h3><?php the_title(); ?><span>.</span></h3>
+				<p><?php the_excerpt(); ?></p>
+				<a href="<?php echo the_permalink(); ?>" class="btn animsition-link">
+					<?php _e( 'Read more', 'pallas' ); ?>
+				</a>
+			</div>
+		</div>
+	</div>
+</section> <!-- .cd-section -->
+<?php wp_reset_postdata(); endif;
