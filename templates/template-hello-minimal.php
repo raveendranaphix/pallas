a0f1406fc5caf617390ab5eb68790ba46b406c24
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-21 15:12:46 +0545

    Added sub folder in hello

---
+++ templates/template-hello-minimal.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Template Name: Minimal page layout
+	Template Post Type: page
+*/
+get_header();
+
+	get_template_part( 'template-parts/hello/minimal/content', 'intro' );
+
+get_footer();
