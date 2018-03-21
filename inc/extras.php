f9816094cb0931fc572f2770dc9580ae548ad7ab
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-21 10:31:58 +0545

    Modified extras.php

--- inc/extras.php
+++ inc/extras.php
@@ -19,34 +19,34 @@ function pallas_is_homepage() {
 function pallas_social_links() {
 	return  array(
 		array(
-			'name' => 'facebook',
-			'label' => __( 'Facebook', 'pallas' ),
-			'title' => 'facebook',
+			'name'     => 'facebook',
+			'label'    => __( 'Facebook', 'pallas' ),
+			'title'    => 'facebook',
 			'icon_hex' => '&#xf09a;'
 		),
 
 		array(
-			'name' => 'twitter',
-			'label' => __( 'Twitter', 'pallas' ),
-			'title' => 'twitter',
+			'name'     => 'twitter',
+			'label'    => __( 'Twitter', 'pallas' ),
+			'title'    => 'twitter',
 			'icon_hex' => '&#xf099;'
 		),
 		array(
-			'name' => 'github',
-			'label' => __( 'GitHub', 'pallas' ),
-			'title' => 'github',
+			'name'     => 'github',
+			'label'    => __( 'GitHub', 'pallas' ),
+			'title'    => 'github',
 			'icon_hex' => '&#xf09b;'
 		),
 		array(
-			'name' => 'google_plus',
-			'label' => __( 'Google +', 'pallas' ),
-			'title' => 'google +',
+			'name'     => 'google_plus',
+			'label'    => __( 'Google +', 'pallas' ),
+			'title'    => 'google +',
 			'icon_hex' => '&#xf0d5;'
 		),
 		array(
-			'name' => 'linkedin',
-			'label' => __( 'LinkedIn', 'pallas' ),
-			'title' => 'linkedin',
+			'name'     => 'linkedin',
+			'label'    => __( 'LinkedIn', 'pallas' ),
+			'title'    => 'linkedin',
 			'icon_hex' => '&#xf0d5;'
 		),
 	);
@@ -54,3 +54,19 @@ function pallas_social_links() {
 
 // Enable excerpt for pages
 add_post_type_support( 'page', 'excerpt' );
+
+// Get the members
+function pallas_get_members() {
+	$args = array(
+		'posts_per_page' => 4,
+		'post_type'      => 'pl_member',
+		'post_status'    => 'publish',
+		'orderby'        => 'date',
+		'order'          => 'desc',
+	);
+
+	// Query
+	$member_query = new WP_Query( $args );
+
+	return $member_query->have_posts() ? $member_query : null;
+}
