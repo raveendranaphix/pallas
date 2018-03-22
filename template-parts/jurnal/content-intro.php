aa1b12415c96bdbc06550ababccabb1d66edd91a
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-22 15:14:08 +0545

    Modified content-intro.php

--- template-parts/jurnal/content-intro.php
+++ template-parts/jurnal/content-intro.php
@@ -1,6 +1,6 @@
 <?php
 
-	$section_page = get_field( 'homepage_section_1', get_the_ID(), true );
+	$section_page = get_field( 'homepage_section_3', get_the_ID(), true );
 	$image  = get_the_post_thumbnail_url( $section_page->ID, 'full' );
 	$text_1 = get_field( 'intro_text_1', get_the_ID(), true );
 	$text_2 = get_field( 'intro_text_2', get_the_ID(), true );
@@ -9,7 +9,7 @@
 	<div class="cd-block-blog">
 		<div class="blog-top" style ="background-image: url( <?php echo $image; ?> )">
 
-			<?php if( ! empty( $text_1 ) ): ?>
+			<?php if( ! empty( $text_1 ) ) : ?>
 				<div class="big-text-pages-top">
 					<?php echo sprintf( '<h1>%s<span>.</span></h1>', $text_1 ); ?>
 				</div>
