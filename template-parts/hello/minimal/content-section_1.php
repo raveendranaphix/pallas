425a68b4b5dfaaf9f0942c07989d8d521a9955d8
Suresh Ghale <chelseasuresh@gmail.com>
2018-03-23 09:05:59 +0545

    Modified content-section_1.php

--- template-parts/hello/minimal/content-section_1.php
+++ template-parts/hello/minimal/content-section_1.php
@@ -1,20 +1,26 @@
+<?php
+
+	$section_1 = get_field( 'minimal section_1', get_the_ID(), true);
+	$image = get_the_post_thumbnail_url( $section_1->ID, 'full' );
+?>
+
 <section class="cd-section" id="scroll-link">
 	<div class="cd-block">
-		<div class="cd-half-block"></div>
+		<div class="cd-half-block" style="background-image: url(<?php echo $image; ?>);">></div>
 		<div class="cd-half-block">
 			<div class="block-text">
+				 <?php foreach ( pallas_contact_info() as $ci ) :
+
+                    // Contact info
+                    $contact = get_theme_mod( 'pallas_contact_info_setting_' . $ci['name'] );
+
+                    if ( ! empty( $contact ) ) :
+                   ?>
 				<div class="contact-details padding-contact">
-					<p><span>&#xf1d8;</span> Email:</p>
-					<h6>office@pallas.com</h6>
+					<p><span><?php echo esc_attr( $ci['icon_hex'] ); ?></span><?php echo esc_attr( $ci['title'] ); ?></p>
+					<h6><?php echo esc_attr( $contact ); ?></h6>
 				</div>
-				<div class="contact-details padding-contact">
-					<p><span>&#xf095;</span> Phone:</p>
-					<h6>0120 (381) 234-294</h6>
-				</div>
-				<div class="contact-details">
-					<p><span>&#xf041;</span> Address:</p>
-					<h6>86-90 Milutina, BG 11000</h6>
-				</div>
+				<?php endif; endforeach; ?>
 			</div>
 		</div>
 	</div>
